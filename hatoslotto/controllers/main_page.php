ab0a1bfdefb18fb9c06117a22f8a4bc558b99106
Danyádi Gergő <A23JE2@hallgato.uni-neumann.hu>
2022-11-27 13:44:00 +0100

    New button, index.php modification

--- hatoslotto/controllers/main_page.php
+++
@@ -1,9 +0,0 @@
-<?php
-    class Main_Page_Controller 
-    {
-        public function main() 
-        {
-            include('views/index.html');
-        }
-    }
-?>
--- hatoslotto/controllers/router.php
+++ hatoslotto/controllers/router.php
@@ -26,12 +26,7 @@
         $target = SERVER_ROOT.'controllers/error404.php';
     }
 
-    $target = SERVER_ROOT.'controllers/'.$controllerfile.'.php';
-    if(! file_exists($target))
-    {
-        $controllerfile = "error404";
-        $target = SERVER_ROOT.'controllers/error404.php';
-    }
+    // TODO: duplikáció, légyszi, ne rakd vissza! Nézd meg a felette lévő 6 sort ;)
 
     include_once($target);
     $class = ucfirst($controllerfile).'_Controller';
